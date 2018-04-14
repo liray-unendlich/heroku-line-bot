07516dd581c076bd4d0889043e064d206343e4a5
liray-unendlich <unendlichkeit629@gmail.com>
2018-04-14 12:44:48 +0900

    add id

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ def message_text(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text)
+        TextSendMessage(text=event.message.id)
     )
 
 
